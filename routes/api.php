92d9a622791e6bb94f0d76d860bde179b32c501c
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2023-10-16 21:09:40 +0700

    add authentication modules

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::post('admin/login', [AuthController::class, 'login']);
+    Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::post('refresh-token', [AuthController::class, 'refresh'])->middleware('auth:api');
 });
