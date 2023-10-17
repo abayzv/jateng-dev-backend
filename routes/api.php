c97923707a76a41306ac1b33dc7419d45c728efa
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2023-10-18 06:08:40 +0700

    add send email module

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\GuestController;
 use App\Http\Controllers\API\PostcardTemplateController;
+use App\Mail\SendEmail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,7 +42,23 @@ Route::prefix('v1')->group(function () {
 
 // Test Route
 Route::get('test', function () {
-    return response()->json([
-        'message' => generateUuid()
-    ]);
+    try {
+        $subject = "Reset Password Request";
+        $content = [
+            'url' => 'http://localhost:8000/reset-password?token=1234567890',
+        ];
+
+        Mail::to('ajipunk008@gmail.com')->send(new SendEmail($subject, 'emails.reset-password', $content));
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Email sent successfully',
+        ]);
+    } catch (\Throwable $th) {
+        Log::error($th->getMessage());
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Email sent failed',
+        ], 500);
+    }
 });
